ffa10759b3b1b5842994cfd390d76ff99d34bb86
slydragonn <alogo18m@gmail.com>
2025-04-29 19:01:04 -0500

    chore: update timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,18 +1,21 @@
-import { EXPERIENCES } from "@/content/experiences"
+interface TimelineProps {
+  date: string
+  title: string
+  description?: string
+}
 
-const Timeline = () => {
+const Timeline = ({ content }: {content: TimelineProps[]}) => {
   return (
-    <section className="flex flex-col max-w-[800px]">
-      <div className="relative border-l-4 border-slate-700 ml-6">
-        {EXPERIENCES.map((experience, index) => (
-          <div key={index} className="relative mb-10 ml-6">
-            <div className="absolute w-4 h-4 bg-slate-700 rounded-full -left-[34px]  top-1.5"></div>
-            <p className="text-sm text-gray-500">{experience.date}</p>
-            <h3 className="text-xl font-semibold">{experience.jobTitle}</h3>
-            <h4 className="text-md text-gray-700 font-medium">
-              {experience.company}
-            </h4>
-            <p className="text-gray-600 mt-2">{experience.description}</p>
+    <section className="flex flex-col w-full max-w-[800px]">
+      <div className="relative border-l-2 border-slate-700 ml-6">
+        {content.map((item) => (
+          <div key={item.title.replaceAll(" ", "")} className="relative mb-10 ml-6">
+            <div className="absolute w-3 h-3 bg-slate-700 rounded-full -left-[31px]  top-1.6"></div>
+            <p className="text-sm text-gray-500">{item.date}</p>
+            <h3 className="text-md text-gray-700 dark:text-gray-200 font-semibold">{item.title}</h3>
+            {
+              item.description ?? <p className="text-gray-600 mt-2">{item.description}</p>
+            }
           </div>
         ))}
       </div>
